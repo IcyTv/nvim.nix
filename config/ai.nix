2f8b9e82e7fbfac21784af715406b55f1afb77f1
Michael Finger <michael.finger@icytv.de>
2026-01-07 15:29:11 +0100

    fix(ai): remove sidekick
    After a trial run, I have concluded that I don't like sidekick.
    I would recommend just using the cli tools. They work better anyways.

--- config/ai.nix
+++ config/ai.nix
@@ -1,25 +1,2 @@
 {...}: {
-  plugins.sidekick = {
-    enable = true;
-    settings = {
-      nes = {
-        enabled = false;
-      };
-    };
-  };
-
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>aa";
-      action = ":Sidekick cli toggle<cr>";
-      options.desc = "Toggle Sidekick";
-    }
-    {
-      mode = "n";
-      key = "<leader>ap";
-      action = ":Sidekick cli prompt<cr>";
-      options.desc = "Sidekick Prompt";
-    }
-  ];
 }
