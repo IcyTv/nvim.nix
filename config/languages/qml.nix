f0782a940614b1829c6e1c1ed4259ddd69413938
Michael Finger <michael.finger@icytv.de>
2026-05-04 14:08:10 +0200

    fix(qml): add command with `-E` flag

--- config/languages/qml.nix
+++ config/languages/qml.nix
@@ -18,6 +18,22 @@ in
       tool = "qmlformat";
       package = pkgs.qt6.qtdeclarative;
     };
+    extraLspOptions = {
+      command = lib.mkOption {
+        type = with lib.types; nullOr (listOf str);
+        default = null;
+        description = "Command override for qmlls.";
+      };
+    };
+    extraConfig = cfg: {
+      plugins.lsp.servers.qmlls.cmd =
+        if cfg.lsp.command != null
+        then cfg.lsp.command
+        else [
+          (if cfg.lsp.package != null then lib.getExe cfg.lsp.package else "qmlls")
+          "-E"
+        ];
+    };
   } {
     inherit pkgs lib config;
   }
