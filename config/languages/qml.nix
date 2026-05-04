82f7088600f85272ebf52eb931ae57741aa34a8c
Michael Finger <michael.finger@icytv.de>
2026-05-04 14:10:36 +0200

    fix(qml): remove reliance on getExe

--- config/languages/qml.nix
+++ config/languages/qml.nix
@@ -8,17 +8,23 @@
 in
   utils.mkLang {
     name = "qml";
-    filetypes = ["qml"];
+    filetypes = ["qml" "qmljs"];
     description = "Enable QML support";
     lsp = {
       server = "qmlls";
       package = pkgs.qt6.qtdeclarative;
+      packageName = "qmlls";
     };
     format = {
       tool = "qmlformat";
       package = pkgs.qt6.qtdeclarative;
     };
     extraLspOptions = {
+      packageName = lib.mkOption {
+        type = lib.types.str;
+        default = lsp.packageName or lsp.server;
+        description = "Executable name inside lsp.package to use.";
+      };
       command = lib.mkOption {
         type = with lib.types; nullOr (listOf str);
         default = null;
@@ -30,7 +36,7 @@ in
         if cfg.lsp.command != null
         then cfg.lsp.command
         else [
-          (if cfg.lsp.package != null then lib.getExe cfg.lsp.package else "qmlls")
+          (if cfg.lsp.package != null then "${cfg.lsp.package}/bin/${cfg.lsp.packageName}" else "qmlls")
           "-E"
         ];
     };
